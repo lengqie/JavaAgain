f459bffc0fe667375b129ea1cbf83cb18af422ac
lengqie <lengqie@foxmail.com>
2022-01-22 00:03:23 +0800

    longest substring without repeating characters

---
+++ src/com/leetcode/plan20day/slidingwindow/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,34 @@
+package com.leetcode.plan20day.slidingwindow;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * 给定一个字符串 s ，请你找出其中不含有重复字符的 最长子串 的长度。
+示例 1:
+
+输入: s = "abcabcbb"
+输出: 3 
+解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+示例 2:
+ */
+public class LongestSubstringWithoutRepeatingCharacters {
+	public static void main(String[] args) {
+//		System.out.println(lengthOfLongestSubstring("aab"));
+		System.out.println(Arrays.toString("bb".split("b")));
+	}
+    public static int lengthOfLongestSubstring(String s) {
+        if (s.length()==0) return 0;
+        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+        int max = 0;
+        int left = 0;
+        for(int i = 0; i < s.length(); i ++){
+            if(map.containsKey(s.charAt(i))){
+                left = Math.max(left,map.get(s.charAt(i)) + 1);
+            }
+            map.put(s.charAt(i),i);
+            max = Math.max(max,i-left+1);
+        }
+        return max;
+    }
+}
