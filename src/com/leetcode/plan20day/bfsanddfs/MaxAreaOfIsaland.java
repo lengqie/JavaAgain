adbce063ad62e8216b2bb242c8128f0397f213a5
lengqie <lengqie@foxmail.com>
2022-01-23 21:28:53 +0800

    max area of island

--- src/com/leetcode/plan20day/bfsanddfs/MaxAreaOfIsaland.java
+++ src/com/leetcode/plan20day/bfsanddfs/MaxAreaOfIsaland.java
@@ -1,10 +1,48 @@
 package com.leetcode.plan20day.bfsanddfs;
 
+/**
+
+给你一个大小为 m x n 的二进制矩阵 grid 。
+
+岛屿 是由一些相邻的 1 (代表土地) 构成的组合，这里的「相邻」要求两个 1 必须在 水平或者竖直的四个方向上 相邻。你可以假设 grid 的四个边缘都被 0（代表水）包围着。
+
+岛屿的面积是岛上值为 1 的单元格的数目。
+
+计算并返回 grid 中最大的岛屿面积。如果没有岛屿，则返回面积为 0 。
+
+ *
+ */
 public class MaxAreaOfIsaland {
 
+	static int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+	//DFS
+    public static int maxAreaOfIsland(int[][] grid) {
+    	int max = 0;
+    	for (int i = 0; i < grid.length; i++) {
+			for (int j = 0; j < grid[0].length; j++) {
+				max = Math.max(dfs(grid,i,j), max);
+			}
+		}
+    	return max;
+    }
+    public static int dfs(int[][]grid,int i, int j) {
+    	// 边界检测
+    	if (i<0 || i >= grid.length || j < 0|| j >= grid[0].length || grid[i][j] ==0) {
+			return 0;
+		}
+    	grid[i][j] = 0;
+    	int count = 1;
+		for(int[] dir:dirs){
+			int dx =i +dir[0];
+			int dy =j +dir[1];
+			count += dfs(grid,dx,dy);
+		}
+    	return count;
+    }
 
 }
