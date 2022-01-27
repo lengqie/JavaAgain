7d0702267b54c6b6ce50d79d3f234d6ab2af765d
lengqie <lengqie@foxmail.com>
2022-01-27 20:12:05 +0800

    reverse linkedlist

---
+++ src/com/leetcode/plan/algorithm/recursion/ReverseLinkedList.java
@@ -0,0 +1,25 @@
+package com.leetcode.plan.algorithm.recursion;
+
+/**
+ * 给你单链表的头节点 head ，请你反转链表，并返回反转后的链表。
+输入：head = [1,2,3,4,5]
+输出：[5,4,3,2,1]
+ */
+
+public class ReverseLinkedList {
+	public class ListNode {
+	    int val;
+	    ListNode next;
+	    ListNode() {}
+	    ListNode(int val) { this.val = val; }
+	    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	}
+    public ListNode reverseList(ListNode head) {
+    	if(head == null || head.next == null)
+    		return head;
+    	 ListNode p = reverseList(head.next);
+    	 head.next.next = head;
+    	 head.next = null;
+    	 return p;
+    }
+}
