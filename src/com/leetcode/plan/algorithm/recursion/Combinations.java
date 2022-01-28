6304e9bcce7e3a599462fbd14fd087d22325574e
lengqie <lengqie@foxmail.com>
2022-01-28 23:04:40 +0800

    combinations

--- src/com/leetcode/plan/algorithm/recursion/Combinations.java
+++ src/com/leetcode/plan/algorithm/recursion/Combinations.java
@@ -1,5 +1,7 @@
 package com.leetcode.plan.algorithm.recursion;
 
+import java.util.*;
+
 /**
  * 给定两个整数 n 和 k，返回范围 [1, n] 中所有可能的 k 个数的组合。
 你可以按 任何顺序 返回答案。
@@ -15,5 +17,24 @@ package com.leetcode.plan.algorithm.recursion;
 ]
  */
 public class Combinations {
+    List<List<Integer>> result = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+    public List<List<Integer>> combine(int n, int k) {
+        combineHelper(n, k, 1);
+        return result;
+    }
 
+    private void combineHelper(int n, int k, int startIndex){
+        //终止条件
+        if (path.size() == k){
+            result.add(new ArrayList<>(path));
+            return;
+        }
+        //         for (int i = startIndex; i <= n - (k - path.size()) + 1; i++){
+         for (int i = startIndex; i <= n; i++){
+            path.add(i);
+            combineHelper(n, k, i + 1);
+            path.removeLast();
+        }
+    }
 }
