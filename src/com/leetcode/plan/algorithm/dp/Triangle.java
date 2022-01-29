56ae1c77a0462f55a83e58b53b810a3643ef3a7c
lengqie <lengqie@foxmail.com>
2022-01-29 22:37:46 +0800

    triangle

---
+++ src/com/leetcode/plan/algorithm/dp/Triangle.java
@@ -0,0 +1,25 @@
+package com.leetcode.plan.algorithm.dp;
+
+import java.util.List;
+
+/**
+ * 给定一个三角形 triangle ，找出自顶向下的最小路径和。
+
+每一步只能移动到下一行中相邻的结点上。相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。
+也就是说，如果正位于当前行的下标 i ，那么下一步可以移动到下一行的下标 i 或 i + 1 。
+ *
+ */
+public class Triangle {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        // dp[i][j] 表示从点 (i, j) 到底边的最小路径和。
+        int[][] dp = new int[n + 1][n + 1];
+        // 从三角形的最后一行开始递推。
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[i][j] = Math.min(dp[i + 1][j], dp[i + 1][j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return dp[0][0];
+    }
+}
