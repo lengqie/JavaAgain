3f526004bd96aa29c906772a9cd870468ce53fc7
lengqie <lengqie@foxmail.com>
2022-02-12 19:49:56 +0800

    reverse print

--- src/com/leetcode/leetbook/illustrationofalgorithm/ReversePrint.java
+++ src/com/leetcode/leetbook/illustrationofalgorithm/ReversePrint.java
@@ -1,5 +1,8 @@
 package com.leetcode.leetbook.illustrationofalgorithm;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
  */
@@ -23,5 +26,22 @@ public class ReversePrint {
 			head = head.next;
 		}
     	return arr;
+    }
+    // 递归
+    List<Integer> list = new ArrayList<Integer>();
+    public int[] reversePrint2(ListNode head) {
+    	recursion(head);
+    	int[] arr = new int[list.size()];
+    	for (int i = 0; i < arr.length; i++) {
+			arr[i] = list.get(i);
+		}
+    	return arr;
+    }
+    public void recursion(ListNode head) {
+    	if(head == null) {
+    		return;
+    	}
+    	recursion(head.next);
+    	list.add(head.val);
     }
 }
