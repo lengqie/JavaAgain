b629f4414150406c344aa6f9b9dfec8271e3e0bd
lengqie <lengqie@foxmail.com>
2022-02-21 23:13:38 +0800

    binary tree postorder traversal fox

--- src/com/leetcode/plan/datastructures/tree/BinaryTreePostorderTraversal.java
+++ src/com/leetcode/plan/datastructures/tree/BinaryTreePostorderTraversal.java
@@ -2,6 +2,8 @@ package com.leetcode.plan.datastructures.tree;
 
 import java.util.*;
 
+import com.leetcode.plan.datastructures.tree.BinaryTreePreorderTraversal.TreeNode;
+
 /**
  给你二叉树的根节点 root ，返回它节点值的 后序 遍历。
  */
@@ -36,5 +38,30 @@ public class BinaryTreePostorderTraversal {
     	// 最后 访问 根节点
     	res.add(root.val);
     }
-   // 迭代法
+   // 迭代法 
+    public List<Integer> postorderTraversal2(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null)
+        	return res;
+
+        Stack<TreeNode> stack = new Stack<>();
+        
+        // 实现 根 -> 右-> 左 (左右访问顺序 与 前序访问不同)
+    	// 先 访问 根节点
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode curr = stack.pop();
+            res.add(curr.val);
+            // 最后访问 右子树 （在栈中 先进后出）
+            if (curr.left != null)
+            	stack.push(curr.left);
+            // 再 访问 左子树 （在栈中 后进先出）
+            if (curr.right != null)
+            	stack.push(curr.right);
+        }
+        // 通过反转 实现 访问为 左->右->根
+        Collections.reverse(res);
+        return res;
+    }
 }
