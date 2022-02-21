7421993f541909797156be28b2cb988125bc2371
lengqie <lengqie@foxmail.com>
2022-02-21 22:36:57 +0800

    binary tree preorder traversal

--- src/com/leetcode/plan/datastructures/tree/BinaryTreePreorderTraversal.java
+++ src/com/leetcode/plan/datastructures/tree/BinaryTreePreorderTraversal.java
@@ -20,7 +20,7 @@ public class BinaryTreePreorderTraversal {
 		         this.right = right;
 		     }
 	}
-	 
+	// 递归 中后 遍历 类似
     public List<Integer> preorderTraversal(TreeNode root) {
     	List<Integer> res = new ArrayList<Integer>();
     	preorder(root,res);
@@ -29,9 +29,35 @@ public class BinaryTreePreorderTraversal {
     public void preorder(TreeNode root,List<Integer> res) {
     	if(root == null)
     		return;
-    	// 
+    	// 先 访问 根节点
     	res.add(root.val);
+    	// 再 访问 左子树
     	preorder(root.left,res);
+    	// 最后访问 右子树
     	preorder(root.right,res);
     }
+    // 迭代 （用栈）
+    public List<Integer> preorderTraversal2(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        if (root == null)
+        	return res;
+
+        Stack<TreeNode> stack = new Stack<>();
+        
+    	// 先 访问 根节点
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            TreeNode curr = stack.pop();
+            res.add(curr.val);
+            //// 再 访问 左子树
+            if (curr.right != null)
+            	stack.push(curr.right);
+         // 最后访问 右子树
+            if (curr.left != null)
+            	stack.push(curr.left);
+        }
+        return res;
+    }
+    
 }
