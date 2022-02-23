cc08998fce5cba496b5df5ef3c0140884b52ba87
lengqie <lengqie@foxmail.com>
2022-02-23 21:46:30 +0800

    maximum depth of binary tree

---
+++ src/com/leetcode/plan/datastructures/tree/MaximumDepthofBinaryTree.java
@@ -0,0 +1,37 @@
+package com.leetcode.plan.datastructures.tree;
+
+/**
+ * 给定一个二叉树，找出其最大深度。
+
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+说明: 叶子节点是指没有子节点的节点。
+ */
+public class MaximumDepthofBinaryTree {
+	 public class TreeNode {
+	     int val;
+	     TreeNode left;
+	     TreeNode right;
+	     TreeNode() {}
+	     TreeNode(int val) { this.val = val; }
+	     TreeNode(int val, TreeNode left, TreeNode right) {
+	         this.val = val;
+	         this.left = left;
+	         this.right = right;
+	     }
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            int leftHeight = maxDepth(root.left);
+            int rightHeight = maxDepth(root.right);
+            return Math.max(leftHeight, rightHeight) + 1;
+        }
+    }
+
+}
